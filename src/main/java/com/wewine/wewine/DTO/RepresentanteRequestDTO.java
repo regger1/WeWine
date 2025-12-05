2d52e50f1ac2a93d4474c69f77f768b1d5db2731
Nathally <nathallyr06@gmail.com>
2025-12-05 17:14:34 -0300

    Representante atualizado

--- src/main/java/com/wewine/wewine/DTO/RepresentanteRequestDTO.java
+++ src/main/java/com/wewine/wewine/DTO/RepresentanteRequestDTO.java
@@ -1,82 +1,82 @@
-package com.wewine.wewine.DTO;
+    package com.wewine.wewine.DTO;
 
-import com.wewine.wewine.enums.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.Data;
+    import com.wewine.wewine.enums.*;
+    import jakarta.validation.constraints.Email;
+    import jakarta.validation.constraints.NotBlank;
+    import jakarta.validation.constraints.NotNull;
+    import jakarta.validation.constraints.Size;
+    import lombok.Data;
 
-import java.time.LocalDate;
+    import java.time.LocalDate;
 
-@Data
-public class RepresentanteRequestDTO {
+    @Data
+    public class RepresentanteRequestDTO {
 
-    @NotBlank
-    private String nome;
+        @NotBlank
+        private String nome;
 
-    @NotBlank
-    private String cpfCnpj;
+        @NotBlank
+        private String cpfCnpj;
 
-    private String rgIe;
+        private String rgIe;
 
-    private LocalDate nascimento;
+        private LocalDate nascimento;
 
-    private String nomeFantasia;
+        private String nomeFantasia;
 
-    private String situacaoLegal;
+        private String situacaoLegal;
 
-    @NotNull
-    private StatusRepresentanteEnum status;
+        @NotNull
+        private StatusRepresentanteEnum status;
 
-    @NotBlank
-    @Email
-    private String email;
+        @NotBlank
+        @Email
+        private String email;
 
-    private String celularWhatsapp;
+        private String celularWhatsapp;
 
-    private String cep;
+        private String cep;
 
-    private String endereco;
+        private String endereco;
 
-    private String numero;
+        private String numero;
 
-    private String complemento;
+        private String complemento;
 
-    private String bairro;
+        private String bairro;
 
-    private String cidade;
+        private String cidade;
 
-    private String estado;
+        private String estado;
 
-    private RegiaoAtuacaoEnum regiaoAtuacao;
+        private RegiaoAtuacaoEnum regiaoAtuacao;
 
-    private PorcentagemEnum regraComissao;
+        private PorcentagemEnum regraComissao;
 
-    private String observacoes;
+        private String observacoes;
 
-    private String banco;
+        private String banco;
 
-    private String agencia;
+        private String agencia;
 
-    private String conta;
+        private String conta;
 
-    private TipoContaEnum tipoConta;
+        private TipoContaEnum tipoConta;
 
-    @NotNull
-    private AcessoAppEnum concederAcessoApp;
+        @NotNull
+        private AcessoAppEnum concederAcessoApp;
 
-    private String loginAplicativo;
+        private String loginAplicativo;
 
-    @NotBlank
-    @Size(min = 6)
-    private String senhaAcesso;
+        @NotBlank
+        @Size(min = 6)
+        private String senhaAcesso;
 
-    private Double vendas;
+        private Double vendas;
 
-    private Double comissao;
+        private Double comissao;
 
-    private Integer clientesAtivos;
+        private Integer clientesAtivos;
 
-    private String[] cidades;
-}
+        private String[] cidades;
+    }
